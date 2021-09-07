5ae632973f1972cc0610e5b0cb5c804301a0b4e0
tj <tomisinjoseph3@gmail.com>
2021-09-07 14:44:22 +0100

    added an image with a border

--- lib/app.rb
+++ lib/app.rb
@@ -8,3 +8,15 @@ end
 get '/secret' do
   'this is a secret'
 end
+
+get '/cat' do
+    "<div style='border: dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png/500/500'>
+    </div>"
+  end
+
+# get '/cat' do
+#   "<body>
+#   <img style='border: dashed red' src='https://i.imgur.com/jFaSxym.png'>
+#   </body>"
+# end
