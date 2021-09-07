16cccab1b28ba522790fa7ea702a1e33c450bdc1
tj <tomisinjoseph3@gmail.com>
2021-09-08 00:08:00 +0100

    used a form element to construct a query string

--- lib/app.rb
+++ lib/app.rb
@@ -11,17 +11,11 @@ end
 
 get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
-  erb(:index)
+  erb :index
 end
 
 get '/named-cat' do
   p params
   @name = params[:name]
-  erb(:index)
+  erb :index
 end
-
-# get '/cat' do
-#   "<body>
-#   <img style='border: dashed red' src='https://i.imgur.com/jFaSxym.png'>
-#   </body>"
-# end
